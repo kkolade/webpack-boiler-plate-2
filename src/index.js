55f3ab8c7bf6cda18bea0a88b9bf36571347c2bb
Kola Kolade <kolakolade@gmail.com>
2022-12-14 11:01:17 +0100

    Configure for style
    Added plugins for css and scss
    configured webpack.config to accept css and scss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+// import "./style.css";
+import "./main.scss";
+
 console.log("Hello webpack!")
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   // entry: { index: path.resolve(__dirname, "index.js")
-  entry: { index: path.resolve(__dirname, "src", "index.js"),
+  mode: 'development',
+  entry: { index: path.resolve(__dirname, "src", "index.js")},
   output: {
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s(a|c)ss)$/,
+        // use: ["style-loader", "css-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader", 'sass-loader'],
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html")
-    })
+    }),
+    new MiniCssExtractPlugin()
   ]
 };
+
